88df4bcdcb56c709478632226f4ec33eb4e3c569
Johanna Horn <j.von.horn@hotmail.com>
2020-11-24 09:55:40 +0100

    hi

---
+++ JS-basics.js
@@ -0,0 +1,17 @@
+/* JavaScript*/
+
+/* basics of 23-11-2020 */
+
+var name "Johanna";
+
+if (confition) {
+    name = "empty";
+
+    var lastname = "Horn";
+}
+
+lastname; // => throws Error (lastname is undefined) beacuse var needs to be defined before statement
+
+
+/* Accessors */
+var name = "Eni";
